d99b06e65fcebc3cf533c33703bfb0432be38714
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-09 15:04:11 +0530

    updated with navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import AddStudent from "./components/AddStudent";
 import Search from "./components/Search";
+import ViewAll from "./components/ViewAll";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <AddStudent />
-      <Search/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddStudent/>}></Route>
+      <Route path='/search' element={<Search/>}></Route>
+      <Route path='/viewall' element={<ViewAll/>}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Navbar from './Navbar';
 
 const Search = () => {
   return (
     <div>
+        <Navbar/>
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <center>
+                <h3>Search Student</h3>
+            </center>
             <div className="row g-3">
               <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <label htmlFor="" className="form-label">
