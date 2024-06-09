019dca2aed386a3ba54027a1557f684a522d94d5
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-09 14:22:51 +0530

    add stud and search

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+const AddStudent = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <center>
+              <h3>Student Registration</h3>
+            </center>
+            <div className="row g-3">
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  First Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Last Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  College
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Date Of Birth
+                </label>
+                <input type="date" name="" id="" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Course
+                </label>
+                <select name="" id="" className="form-control">
+                  <option value="MCA">MCA</option>
+                  <option value="MBA">MBA</option>
+                  <option value="CSE">CSE</option>
+                  <option value="EEE">EEE</option>
+                  <option value="ECE">ECE</option>
+                  <option value="MECH">MECH</option>
+                </select>
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Mobile
+                </label>
+                <input type="number" name="" id="" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Email
+                </label>
+                <input type="email" name="" id="" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Address
+                </label>
+                <textarea name="" id="" className="form-control"></textarea>
+              </div>
+              <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <center>
+                  <button className="btn btn-success">Submit</button> 
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddStudent;
